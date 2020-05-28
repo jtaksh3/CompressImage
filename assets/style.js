41b8d1f4bdf428c58d317d90cfeb6cc10e2df1fd
jtaksh3 <jtaksh3@gmail.com>
2020-05-28 17:30:02 +0530

    SIgnup Update

--- assets/style.js
+++ assets/style.js
@@ -37,3 +37,259 @@ $(".tab a").on("click", function (e) {
 
   $(target).fadeIn(600);
 });
+
+//------------------------------------------------ ALERT START ---------------------------------------------------------
+
+function showError(msg) {
+    $(".alert")
+        .addClass("error-alert")
+        .text(msg)
+        .animate(
+            {
+                top: "40px",
+                opacity: 1
+            },
+            500
+        );
+
+    setTimeout(function() {
+        $(".alert").animate(
+            {
+                top: "0px",
+                opacity: 0
+            },
+            500,
+            function() {
+                $(".alert").removeClass("error-alert");
+            }
+        );
+    }, 5000);
+}
+
+function showSuccess(msg) {
+    $(".alert")
+        .addClass("success-alert")
+        .text(msg)
+        .animate(
+            {
+                top: "40px",
+                opacity: 1
+            },
+            500
+        );
+
+    setTimeout(function() {
+        $(".alert").animate(
+            {
+                top: "0px",
+                opacity: 0
+            },
+            500,
+            function() {
+                $(".alert").removeClass("success-alert");
+            }
+        );
+    }, 5000);
+}
+
+//------------------------------------------------- ALERT END ----------------------------------------------------------
+
+//-------------------------------------------- ONBLUR VALIDATION START --------------------------------------------------
+
+let text_constraint = /^[A-Za-z]+$/;
+var email_constraint = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+var password_constraint = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+
+function validateName(input) {
+
+  if(!input.value.trim().length || !text_constraint.test(input.value)) {
+      //RED BORDER
+      input.style.borderColor = "#e74c3c";
+  }
+  //Default Border
+  else {
+    input.value = input.value[0].toUpperCase() + input.value.slice(1).toLowerCase();
+    input.style.borderColor = "#a0b3b0";
+  }
+}
+
+function validateEmail(input) {
+
+        if (email_constraint.test(input.value)) {
+          //Default border
+          input.style.borderColor = "#a0b3b0";
+          input.value = input.value.toLowerCase();
+
+         } else {
+          //red border
+          input.style.borderColor = "#e74c3c";
+        }
+      
+    }
+
+function validatePassword(input) {
+
+  if (password_constraint.test(input.value)) {
+
+    //Default Border
+    input.style.borderColor = "#a0b3b0";
+
+  } else {
+    //red border
+    input.style.borderColor = "#e74c3c";
+  }
+}
+
+function validateCPassword(input) {
+  var first = document.getElementsByName("signup-password").value;
+
+    if (input.value == first && password_constraint.test(input.value)) {
+      //Default border
+      input.style.borderColor = "#a0b3b0";
+
+      } else {
+      //red border
+      input.style.borderColor = "#e74c3c";
+    }
+}
+
+//-------------------------------------------- ONBLUR VALIDATION END ----------------------------------------------------
+
+//--------------------------------------- FORM VALIDATION ON SUBMIT START -----------------------------------------------
+
+function validateFields(fname, lname, email, password, cpassword) {
+
+  if (!$.trim(fname) || !$.trim(lname) || !$.trim(email) || !$.trim(password) || !$.trim(cpassword)) {
+      return 'EMPTY_FIELDS';
+  }
+  if(!text_constraint.test(fname)) {
+    return 'INVALID_FNAME';
+  }
+  if(!text_constraint.test(lname)) {
+    return 'INVALID_LNAME';
+  }
+  if(!email_constraint.test(email)) {
+    return 'INVALID_EMAIL';
+  }
+  if(!password_constraint.test(password)) {
+    return 'INVALID_PASSWORD';
+  }
+  if(!(cpassword == password)) {
+    return 'PASSWORD_DIFFERENT';
+  }
+}
+
+//--------------------------------------- FORM VALIDATION ON SUBMIT START -----------------------------------------------
+
+//----------------------------------------- SIGNING UP THE USER START ---------------------------------------------------
+
+$("#signup-btn").on("click", function(event) {
+    event.preventDefault();
+    // Validate signup form details here
+    let fname = $("#signup-fname").val();
+    let lname = $("#signup-lname").val();
+    let email = $("#signup-email").val();
+    let password = $("#signup-password").val();
+    let cpassword = $("#signup-cpassword").val();
+
+    let validateResponse = validateFields(fname, lname, email, password, cpassword);
+
+    if(validateResponse == 'EMPTY_FIELDS') {
+      showError("Empty Fields!!!");
+      return false;
+    }
+    else if(validateResponse == 'INVALID_FNAME') {
+      showError("The Provided First Name is not a Valid First Name");
+      return false;
+    }
+    else if(validateResponse == 'INVALID_LNAME') {
+      showError("The Provided Last Name is not a Valid Last Name");
+      return false;
+    }
+    else if(validateResponse == 'INVALID_EMAIL') {
+      showError("The Provided Email is not a Valid Email");
+      return false;
+    }
+    else if(validateResponse == 'INVALID_PASSWORD') {
+      showError("The Provided Password is not a Valid Password");
+      return false;
+    }
+    else if(validateResponse == 'PASSWORD_DIFFERENT'){
+      showError("The Provided Confirm Password doesn't Matched")
+      return false;
+    }
+
+    //AJAX request for signup form
+    $.ajax({
+        url: "./bin/login-register/signup.php",
+        method: "POST",
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify({
+            fname: fname,
+            lname: lname,
+            email: email,
+            password: password
+        }),
+        beforeSend: function() {
+            //Show loader before sending ajax request
+            $("#signup-btn")
+                .html('<i class="fa fa-circle-o-notch fa-spin"></i>   Signing up')
+                .css("pointer-events", "none");
+            $("#signup-fname").val("");
+            $("#signup-lname").val("");
+            $("#signup-email").val("");
+            $("#signup-password").val("");
+            $("#signup-cpassword").val("");
+        },
+        success: function(response) {
+
+            $("#signup-btn")
+                .html(
+                    '<i class="fa fa-user-plus" aria-hidden="true"></i> Sign up'
+                )
+                .css("pointer-events", "auto");
+
+            $("#signup-fname").val("");
+            $("#signup-lname").val("");
+            $("#signup-email").val("");
+            $("#signup-password").val("");
+            $("#signup-cpassword").val("");
+
+            if (response.code == "SIGNUP_SUCCESS") {
+                //Allow user to login now
+                showSuccess(
+                    "Successfully Signed up ! "
+                );
+            } else if (response.code == "USER_ALREADY_EXIST") {
+                //User is already registered
+                showError(
+                    "User Already Exist"
+                );
+
+            } else if (response.code == "FORM_NOT_SUBMITTED") {
+                //User has not submitted the form
+                showError("Server error. Try again later.");
+
+            } else {
+                //Server error
+                showError("Server error. Try again later.");
+
+            }
+        },
+        error: function(request, error) {
+            // showError("Server error. Try again later.");
+            //Hide loader after receiving request
+            $("#signup-btn")
+                .html(
+                    '<i class="fa fa-user-plus" aria-hidden="true"></i> Sign up'
+                )
+                .css("pointer-events", "auto");
+            showError("Server error. Try again later.");
+
+        }
+    });
+});
+
+//------------------------------------------ SIGNING UP THE USER END ---------------------------------------------------
+
